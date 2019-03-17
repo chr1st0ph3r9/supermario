798f6b819baf52d6a721ddfb3ca7f326b23b9fdb
chr1st0ph3r9 <christopherhayling9@gmail.com>
2019-03-17 18:50:01 -0500

    first commit

---
+++ Assets/script/Enemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+    bool CambioPos;
+    float speed = 3;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("ob"))
+        {
+            CambioPos = ! CambioPos;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CambioPos == true)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (CambioPos == false)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+	}
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed = 5;
+    public float JumpForce = 7;
+    Rigidbody2D Rig;
+    private bool CompareFloor;
+    Animator Anim;
+
+    void Start()
+    {
+        Rig = transform.GetComponent<Rigidbody2D>();
+        Anim = transform.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        transform.Translate(x, 0, 0);
+        
+        if (x > 0)
+        {
+         
+            transform.GetComponent<SpriteRenderer>().flipX = false;
+            Anim.SetBool("walk", true);
+        }
+        if (x < 0)
+        {
+           
+            transform.GetComponent<SpriteRenderer>().flipX = true;
+            Anim.SetBool("walk", true);
+        }
+        if(x==0)
+        {
+            Debug.Log("static");
+            Anim.SetBool("walk", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&& CompareFloor)
+        {
+            Rig.AddForce(Vector2.up *JumpForce,ForceMode2D.Impulse);
+            Anim.SetBool("jump", true);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("floor")|| collision.transform.CompareTag("ob")) 
+        {
+            CompareFloor = true;
+            Anim.SetBool("jump", false);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("floor")|| collision.transform.CompareTag("ob")) 
+        {
+            CompareFloor = false;
+        }
+    }
+}
